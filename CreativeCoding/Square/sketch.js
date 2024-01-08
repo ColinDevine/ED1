c417985ab1cefa61bd4077e1e5579b68ce901616
Colin Devine <colintdevine@gmail.com>
2024-01-08 08:53:17 -0500

    update

--- CreativeCoding/Square/sketch.js
+++ CreativeCoding/Square/sketch.js
@@ -1,7 +1,16 @@
 function setup() { //runs one time
-
+  createCanvas(720,720);
+  background(255,0,0);
+  fill("yellow");
+  strokeWeight(8);
 }
 
 function draw() { //runs in a loop
-
+  translate(60,60); //resets origin
+  quad(
+    0,0,
+    300,0,
+    300,300,
+    0,300
+  );
 }
