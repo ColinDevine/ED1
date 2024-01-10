94710b8aec413f99946e466264668bca8a05ce82
Colin Devine <colintdevine@gmail.com>
2024-01-10 13:46:27 -0500

    update

---
+++ CreativeCoding/finalBG/sketch.js
@@ -0,0 +1,27 @@
+//adding a GIF to the grid
+let gif;
+function preload('assets/DevineGif.gif');
+
+function setup() { //runs one time
+  createCanvas(windowWidth,windowHeight);
+  fill("grey");
+  strokeWeight(1);
+}
+
+function draw() { //runs in a loop
+
+	background('lightgrey')
+  
+  var numSides = 10;
+  var sideLen = windowWidth/numSides;
+  
+  for(var x = 0;x<windowWidth;x+=sideLen){
+      for(var y =0;y<windowHeight;y+=sideLen){
+          image(gif, x, y, sideLen, sideLen);
+      }
+  }
+}
+
+function windowResized(){
+  resize(windowWidth, windowHeight); 
+}
